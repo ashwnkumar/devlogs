ca1c5855fd3f75f375203cba3f494f697551f1c4
Ashwin Kumar <ashwin12kumar07@gmail.com>
2026-02-20 14:31:18 +0530

    maintenance update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    const inMaintenance = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
+
+    if (inMaintenance) {
+      return [
+        {
+          source:
+            "/((?!maintenance|api|_next/static|_next/image|favicon.ico).*)",
+          destination: "/maintenance",
+          permanent: false,
+        },
+      ];
+    }
+
+    return [];
+  },
 };
 
 export default nextConfig;
